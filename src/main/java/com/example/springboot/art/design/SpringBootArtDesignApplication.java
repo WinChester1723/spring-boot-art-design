e3b8d8cd4ce42455ff535efe0f533725af983b21
Pasha <qordon23@gmail.com>
2022-07-22 01:53:01 +0400

    new create project for presentation

---
+++ src/main/java/com/example/springboot/art/design/SpringBootArtDesignApplication.java
@@ -0,0 +1,13 @@
+package com.example.springboot.art.design;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootArtDesignApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBootArtDesignApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/springboot/art/design/SpringBootArtDesignApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.springboot.art.design;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBootArtDesignApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
